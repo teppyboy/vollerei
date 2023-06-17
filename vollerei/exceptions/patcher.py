437c9a5e7634fb9679c2b01f6aae2b63c1b7b380
tretrauit <tretrauit@gmail.com>
2023-06-17 09:20:57 +0700

    feat: add more exceptions

--- vollerei/exceptions/patcher.py
+++ vollerei/exceptions/patcher.py
@@ -11,3 +11,21 @@ class VersionNotSupportedError(PatcherError):
     """Exception raised when the game version is not supported."""
 
     pass
+
+
+class PatchingFailedError(PatcherError):
+    """Exception raised when the patching failed."""
+
+    pass
+
+
+class PatchUpdateError(PatcherError):
+    """Exception raised when the patch update failed."""
+
+    pass
+
+
+class UnpatchingFailedError(PatcherError):
+    """Exception raised when the unpatching failed."""
+
+    pass
