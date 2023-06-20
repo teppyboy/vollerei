a3a6cfdbe36dd60de8213c06366408e4be304c62
tretrauit <tretrauit@gmail.com>
2023-06-20 18:51:21 +0700

    cli: Y/n instead of y/n

--- vollerei/cli/utils.py
+++ vollerei/cli/utils.py
@@ -6,8 +6,8 @@ def ask(question: str):
         print(question + " [Y/n] Y")
         return True
     while True:
-        answer = input(question + " [y/n] ")
-        if answer.lower() in ["y", "yes"]:
+        answer = input(question + " [Y/n] ")
+        if answer.lower().strip() in ["y", "yes", ""]:
             return True
         # Pacman way, treat all other answers as no
         else:
